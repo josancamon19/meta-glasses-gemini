c485bc3e69f3eb2c852fd4503a096191226a0f82
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-04-08 14:32:01 -0500

    google_search_pipeline try catch added

--- functionality/search.py
+++ functionality/search.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 from utils.gemini import *
 from utils.redis_utils import *
 import requests
+from tenacity import retry, stop_after_attempt
 
 
+@retry(stop=stop_after_attempt(2))
 def get_organic_results_serper_dev(query, num_results=3, location="United States"):
     print('get_organic_results_serper_dev', query, num_results, location)
     if cached := get_generic_cache('get_organic_results_serper_dev:' + query):
@@ -22,6 +24,7 @@ def get_organic_results_serper_dev(query, num_results=3, location="United States
     return urls
 
 
+@retry(stop=stop_after_attempt(2))
 def scrape_website_crawlbase(url: str):
     try:
         response = requests.get(f'https://api.crawlbase.com/?token={os.getenv("CRAWLBASE_API_KEY")}&url=' + url)
@@ -71,6 +74,8 @@ def google_search_pipeline(message: str):
     google_search_query = generate_google_search_query(message)
     print('google_search_query', google_search_query)
     news_urls = get_organic_results_serper_dev(google_search_query)
+    if not news_urls:
+        return "No news data found for this query"
     news_data = []
     scrape_urls_threaded(news_data, news_urls)
     if not news_data:
