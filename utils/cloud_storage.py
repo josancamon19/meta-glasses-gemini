c5de1f97ce5e7b531d64b2184aeda68594e151af
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-04-10 09:18:20 -0500

    upload_image cloud storage commented temp

--- utils/cloud_storage.py
+++ utils/cloud_storage.py
@@ -2,14 +2,15 @@ import os
 
 from google.cloud import storage
 
-print('GOOGLE_APPLICATION_CREDENTIALS:', os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-storage_client = storage.Client.from_service_account_json(os.getenv('GOOGLE_APPLICATION_CREDENTIALS', 'google-credentials.json'))
-bucket_name = os.getenv('CLOUD_STORAGE_BUCKET_NAME')
+# print('GOOGLE_APPLICATION_CREDENTIALS:', os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+# storage_client = storage.Client.from_service_account_json(os.getenv('GOOGLE_APPLICATION_CREDENTIALS', 'google-credentials.json'))
+# bucket_name = os.getenv('CLOUD_STORAGE_BUCKET_NAME')
 
 
 def upload_image(path: str):
     print('upload_image:', path)
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(path)
-    blob.upload_from_filename(path, if_generation_match=0)
-    return f'https://storage.googleapis.com/{bucket_name}/{path}'
+    # bucket = storage_client.bucket(bucket_name)
+    # blob = bucket.blob(path)
+    # blob.upload_from_filename(path, if_generation_match=0)
+    # return f'https://storage.googleapis.com/{bucket_name}/{path}'
+    return ''
