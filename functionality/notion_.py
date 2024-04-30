b4d0280a344cfffef40e0f2eadc4213ca1b6e306
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-04-30 15:57:18 -0500

    notion add_new_page fix

--- functionality/notion_.py
+++ functionality/notion_.py
@@ -1,22 +1,31 @@
 import os
 
+import requests
 from notion_client import Client
 
 # Your Notion credentials
-DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
 client = Client(auth=os.getenv('NOTION_INTEGRATION_SECRET'))
+db_id = os.getenv('NOTION_DATABASE_ID')
 
 
-def add_new_page(title, category, content):
-    new_page = client.pages.create(
-        parent={"type": "database_id", "database_id": DATABASE_ID},
-        properties={
+def add_new_page(title: str, category: str, content: str):
+    data = {
+        "parent": {"database_id": 'abff2f35712b49a28379f595be2706e4'},
+        "properties": {
             "Title": {"title": [{"text": {"content": title}}]},
             "Category": {"select": {"name": category}},
             "Content": {"rich_text": [{"text": {"content": content}}]}
         }
-    )
+    }
+    requests.post('https://api.notion.com/v1/pages', json=data, headers={
+        'Authorization': f'Bearer {os.getenv("NOTION_INTEGRATION_SECRET")}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Notion-Version': '2022-06-28'
+    })
     print(f"New Notion page created with title: {title}")
     # TODO: write any detail to this page, or related terms (suggested by Gemini)
 
+
 # TODO: implement function to retrieve pages where are about X, and return in simple message what have been told
+# add_new_page('Test', 'Personal', 'This is a test page')
